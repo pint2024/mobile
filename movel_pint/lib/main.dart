b150b8ed63e991b3ae496f05dd3322655bb05ab4
Daniel <danielvalpereiro@gmail.com>
2024-04-03 23:03:26 +0100

    Main

--- movel_pint/lib/main.dart
+++ movel_pint/lib/main.dart
@@ -20,6 +20,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
+  PageController _pageController = PageController(initialPage: 0);
 
   void _onItemTapped(int index) {
     setState(() {
@@ -27,19 +28,37 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void _nextPage() {
+    if (_pageController.page! < 2) {
+      _pageController.nextPage(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.ease,
+      );
+    }
+  }
+
+  void _previousPage() {
+    if (_pageController.page! > 0) {
+      _pageController.previousPage(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.ease,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60), 
+        preferredSize: Size.fromHeight(60),
         child: AppBar(
-          backgroundColor: const Color.fromRGBO(57, 99, 156, 1.0), 
+          backgroundColor: const Color.fromRGBO(57, 99, 156, 1.0),
           title: Row(
             children: [
               Padding(
-                padding: EdgeInsets.only(left: 16), 
+                padding: EdgeInsets.only(left: 16),
                 child: Image.asset(
-                  'assets/Images/logo.png', 
+                  'assets/Images/logo.png',
                   width: 40,
                   height: 40,
                 ),
@@ -48,10 +67,8 @@ class _HomePageState extends State<HomePage> {
                 child: Align(
                   alignment: Alignment.centerRight,
                   child: IconButton(
-                    icon: Icon(Icons.account_circle, color: Colors.white), 
-                    onPressed: () {
-                      
-                    },
+                    icon: Icon(Icons.account_circle, color: Colors.white),
+                    onPressed: () {},
                   ),
                 ),
               ),
@@ -60,31 +77,96 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: Center(
-        child: Text('Conteúdo da página $_selectedIndex'),
+        child: Container(
+          alignment: Alignment.topCenter,
+          child: SizedBox(
+            width: 900,
+            height: 240,
+            child: Stack(
+              children: [
+                PageView(
+                  controller: _pageController,
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Container(
+                      width: 300, 
+                      height: 200, 
+                      child: Image.network(
+                          'https://t4.ftcdn.net/jpg/03/84/55/29/360_F_384552930_zPoe9zgmCF7qgt8fqSedcyJ6C6Ye3dFs.jpg'),
+                    ),
+                    Container(
+                      width: 300, 
+                      height: 200, 
+                      child: Image.network(
+                          'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/RE2wSVH_RE4dchU:VP1-539x349?resMode=sharp2&op_usm=1.5,0.65,15,0&qlt=90&fmt=png-alpha'),
+                    ),
+                    Container(
+                      width: 300, 
+                      height: 200, 
+                      child: Image.network(
+                          'https://img.freepik.com/fotos-gratis/arvore-solitaria_181624-46361.jpg?w=1060&t=st=171812141722~exp=1712142322~hmac=a0f37d64a060617c99ed647fab3b10e1b8e770402f9cdcfcbaaf92b879e20aa7'),
+                    ),
+                  ],
+                ),
+                Positioned(
+                  top: 80,
+                  left: 1,
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_back, size: 20),
+                    onPressed: _previousPage,
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                  ),
+                ),
+                Positioned(
+                  top: 80,
+                  right: 1,
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_forward, size: 20),
+                    onPressed: _nextPage,
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                  ),
+                ),
+              ],
+            ),
+            
+          ),
+        ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_month),
-            label: 'Calendario',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notification_add),
-            label: 'Notificações',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: 'ChatRoom',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Definições',
+      bottomNavigationBar: Column(
+  mainAxisSize: MainAxisSize.min,
+  children: [
+    Container(
+      margin: EdgeInsets.only(bottom: 30), // Define a margem inferior desejada
+      child: Divider(
+        color: const Color.fromARGB(255, 255, 255, 255),
+        thickness: 3,
+      ),
+    ),
+          BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_month),
+                label: 'Calendario',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.notification_add),
+                label: 'Notificações',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.chat),
+                label: 'ChatRoom',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: 'Definições',
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.blue,
+            unselectedItemColor: Colors.grey,
+            onTap: _onItemTapped,
           ),
         ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,        
-        onTap: _onItemTapped,
       ),
     );
   }
