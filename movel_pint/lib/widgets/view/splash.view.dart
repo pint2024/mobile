646b8bdf5c5c946096c209e58ff947c04ea64dab
LatifC23 <131891707+LatifC23@users.noreply.github.com>
2024-05-13 23:07:17 +0100

    login

---
+++ movel_pint/lib/widgets/view/splash.view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+
+class SplashView extends StatelessWidget{
+  const({ Key? key}) : super(key: key);
+
+
+  @override 
+  Widget build(BuildContext context){
+    return Container(
+
+
+    );
+  }
+
+}
