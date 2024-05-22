d99ea37f80184fa104e4ae2a093ca65a7ad05d00
Francisco Meneses <marques3033@gmail.com>
2024-05-22 12:50:20 +0100

    login
    sou lindo

--- movel_pint/lib/perfil/login.dart
+++ movel_pint/lib/perfil/login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:ionicons/ionicons.dart';
 import 'registo.dart'; // Importe o arquivo registro.dart
 
 void main() {
@@ -38,10 +40,10 @@ class LoginPage extends StatelessWidget {
         ),
       ),
       body: Container(
-        color: const Color.fromRGBO(57, 99, 156, 1.0), // Isso faz com que o fundo atrás da caixa seja azul
+        color: const Color.fromRGBO(57, 99, 156, 1.0),
         child: Center(
           child: Transform.scale(
-            scale: 0.9, // Isso diminui o tamanho do widget por um fator de 3
+            scale: 0.9,
             child: Container(
               padding: EdgeInsets.all(10),
               decoration: BoxDecoration(
@@ -52,7 +54,7 @@ class LoginPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Padding(
-                    padding: EdgeInsets.only(bottom: 20.0, right: 10.0), // Adicione esta linha
+                    padding: EdgeInsets.only(bottom: 20.0, right: 10.0),
                     child: Text(
                       'Login in to your Account',
                       style: TextStyle(
@@ -65,7 +67,7 @@ class LoginPage extends StatelessWidget {
                   TextField(
                     decoration: InputDecoration(
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0), // Adicione esta linha
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
                       labelText: 'Email',
                     ),
@@ -75,7 +77,7 @@ class LoginPage extends StatelessWidget {
                     obscureText: true,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0), // Adicione esta linha
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
                       labelText: 'Password',
                     ),
@@ -83,14 +85,14 @@ class LoginPage extends StatelessWidget {
                   SizedBox(height: 10),
                   SwitchListTile(
                     title: Text('Lembrar-se do login'),
-                    value: false, // Você pode substituir isso por uma variável que mantém o estado do switch
+                    value: false,
                     onChanged: (bool value) {
                       // Adicione aqui a lógica para lidar com a alteração do estado do switch
                     },
                   ),
                   SizedBox(height: 10),
                   Container(
-                    width: double.infinity, // Isso faz com que o botão tenha a largura máxima disponível
+                    width: double.infinity,
                     child: ElevatedButton(
                       style: ButtonStyle(
                         backgroundColor: MaterialStateProperty.all<Color>(Color.fromRGBO(57, 99, 156, 1.0)),
@@ -106,25 +108,50 @@ class LoginPage extends StatelessWidget {
                       ),
                     ),
                   ),
-                  SizedBox(height: 20),
-                  OutlinedButton(
-                    onPressed: () {
-                      _handleSignInWithGoogle(context);
-                    },
-                    child: Text('Login com Google'),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        icon: Transform.scale(
+                          scale: 1.5, // Ajuste o tamanho conforme necessário
+                          child: Icon(
+                            Ionicons.logo_google,
+                            color: Colors.red,
+                            size: 30,
+                          ),
+                        ),
+                        onPressed: () {
+                          _handleSignInWithGoogle(context);
+                        },
+                      ),
+                      SizedBox(width: 20), // Espaçamento entre os ícones
+                      IconButton(
+                        icon: Transform.scale(
+                          scale: 1.5, // Ajuste o tamanho conforme necessário
+                          child: Icon(
+                            Ionicons.logo_facebook,
+                            color: Colors.blue,
+                            size: 30,
+                          ),
+                        ),
+                        onPressed: () {
+                          _handleSignInWithFacebook(context);
+                        },
+                      ),
+                    ],
                   ),
-                  Spacer(), // Adiciona um espaço flexível
-                  // Botão para ir para a página de registro
+                  Spacer(),
                   TextButton(
                     onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => RegisterPage()), // Página de registro
+                        MaterialPageRoute(builder: (context) => RegisterPage()),
                       );
                     },
                     child: Text(' Não tens uma conta? Registar'),
                   ),
-                  SizedBox(height: 20), // Adiciona um espaço fixo no final
+                  SizedBox(height: 20),
                 ],
               ),
             ),
@@ -136,22 +163,30 @@ class LoginPage extends StatelessWidget {
 
   Future<void> _handleSignInWithGoogle(BuildContext context) async {
     try {
-      // Configure o GoogleSignIn
       GoogleSignIn googleSignIn = GoogleSignIn(scopes: ['email']);
-
-      // Autentique o usuário com o Google
       GoogleSignInAccount? googleUser = await googleSignIn.signIn();
 
       if (googleUser != null) {
-        // O usuário está autenticado com o Google, você pode prosseguir com o login
-        // ou armazenar as credenciais do usuário, conforme necessário.
         print('Usuário autenticado com sucesso: ${googleUser.email}');
       } else {
-        // O usuário cancelou o processo de login com o Google
         print('Falha ao autenticar com o Google');
       }
     } catch (error) {
       print('Erro ao autenticar com o Google: $error');
+    }
+  }
+
+  Future<void> _handleSignInWithFacebook(BuildContext context) async {
+    try {
+      final LoginResult result = await FacebookAuth.instance.login(); // by default we request the email and the public profile
+      if (result.status == LoginStatus.success) {
+        final AccessToken accessToken = result.accessToken!;
+        print('Usuário autenticado com sucesso no Facebook');
+      } else {
+        print('Falha ao autenticar com o Facebook: ${result.status}');
+      }
+    } catch (error) {
+      print('Erro ao autenticar com o Facebook: $error');
     }
   }
 }
