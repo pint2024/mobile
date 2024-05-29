6dc5cac8ccd3f127655cec274584689e336bcec6
JoaoSantos12f <joao12fcp@gmail.com>
2024-05-29 10:19:25 +0100

    update criarEvento

--- movel_pint/lib/evento/criarEvento.dart
+++ movel_pint/lib/evento/criarEvento.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -80,197 +79,198 @@ class _EventFormPageState extends State<EventFormPage> {
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              'Criar Novo Evento',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Criar Novo Evento',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            Form(
-              key: _formKey,
-              child: ListView(
-                shrinkWrap: true,
-                children: [
-                  if (_image != null)
-                    Image.file(
-                      _image!,
-                      height: 200,
-                      fit: BoxFit.cover,
-                    ),
-                  ElevatedButton(
-                    onPressed: _getImage,
-                    child: Text('Selecionar Imagem'),
-                  ),
-                  TextFormField(
-                    controller: _nameController,
-                    decoration: InputDecoration(
-                      labelText: 'Nome do Evento',
-                      border: OutlineInputBorder(),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor, insira o nome do evento';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    controller: _locationController,
-                    decoration: InputDecoration(
-                      labelText: 'Local',
-                      border: OutlineInputBorder(),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor, insira o local do evento';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    controller: _descriptionController,
-                    decoration: InputDecoration(
-                      labelText: 'Descrição',
-                      border: OutlineInputBorder(),
-                    ),
-                    maxLines: 3,
-                  ),
-                  SizedBox(height: 10),
-                  DropdownButtonFormField<String>(
-                    value: _selectedSubtopic,
-                    decoration: InputDecoration(
-                      labelText: 'Subtópico',
-                      border: OutlineInputBorder(),
-                    ),
-                    items: _subtopics.map((String subtopic) {
-                      return DropdownMenuItem<String>(
-                        value: subtopic,
-                        child: Text(subtopic),
-                      );
-                    }).toList(),
-                    onChanged: (newValue) {
-                      setState(() {
-                        _selectedSubtopic = newValue;
-                      });
-                    },
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor, selecione um subtópico';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: TextFormField(
-                          readOnly: true,
-                          controller: _selectedDate != null
-                              ? TextEditingController(text: '${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}')
-                              : null,
-                          decoration: InputDecoration(
-                            labelText: 'Dia',
-                            border: OutlineInputBorder(),
-                          ),
-                          validator: (value) {
-                            if (_selectedDate == null) {
-                              return 'Por favor, selecione o dia do evento';
-                            }
-                            return null;
-                          },
-                        ),
+              SizedBox(height: 20),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    if (_image != null)
+                      Image.file(
+                        _image!,
+                        height: 200,
+                        fit: BoxFit.cover,
                       ),
-                      IconButton(
-                        icon: Icon(Icons.calendar_today),
-                        onPressed: _selectDate,
+                    ElevatedButton(
+                      onPressed: _getImage,
+                      child: Text('Selecionar Imagem'),
+                    ),
+                    TextFormField(
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        labelText: 'Nome do Evento',
+                        border: OutlineInputBorder(),
                       ),
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: TextFormField(
-                          readOnly: true,
-                          controller: _selectedTime != null
-                              ? TextEditingController(text: '${_selectedTime!.hour}:${_selectedTime!.minute}')
-                              : null,
-                          decoration: InputDecoration(
-                            labelText: 'Hora',
-                            border: OutlineInputBorder(),
-                          ),
-                          validator: (value) {
-                            if (_selectedTime == null) {
-                              return 'Por favor, selecione a hora do evento';
-                            }
-                            return null;
-                          },
-                        ),
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.access_time),
-                        onPressed: _selectTime,
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        String name = _nameController.text;
-                        String location = _locationController.text;
-                        String description = _descriptionController.text;
-                        String subtopic = _selectedSubtopic!;
-
-                        DateTime? dateTime;
-                        if (_selectedDate != null && _selectedTime != null) {
-                          dateTime = DateTime(
-                            _selectedDate!.year,
-                            _selectedDate!.month,
-                            _selectedDate!.day,
-                            _selectedTime!.hour,
-                            _selectedTime!.minute,
-                          );
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, insira o nome do evento';
                         }
-
-                        // Exemplo de uso dos dados (pode ser enviado para um serviço, etc.)
-                        print('Nome do Evento: $name');
-                        print('Local: $location');
-                        print('Descrição: $description');
-                        print('Subtópico: $subtopic');
-                        if (dateTime != null) {
-                          print('Data e Hora: $dateTime');
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      controller: _locationController,
+                      decoration: InputDecoration(
+                        labelText: 'Local',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, insira o local do evento';
                         }
-
-                        // Limpar formulário após envio
-                        _nameController.clear();
-                        _locationController.clear();
-                        _descriptionController.clear();
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      controller: _descriptionController,
+                      decoration: InputDecoration(
+                        labelText: 'Descrição',
+                        border: OutlineInputBorder(),
+                      ),
+                      maxLines: 3,
+                    ),
+                    SizedBox(height: 10),
+                    DropdownButtonFormField<String>(
+                      value: _selectedSubtopic,
+                      decoration: InputDecoration(
+                        labelText: 'Subtópico',
+                        border: OutlineInputBorder(),
+                      ),
+                      items: _subtopics.map((String subtopic) {
+                        return DropdownMenuItem<String>(
+                          value: subtopic,
+                          child: Text(subtopic),
+                        );
+                      }).toList(),
+                      onChanged: (newValue) {
                         setState(() {
-                          _selectedDate = null;
-                          _selectedTime = null;
-                          _image = null;
-                          _selectedSubtopic = null;
+                          _selectedSubtopic = newValue;
                         });
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, selecione um subtópico';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            readOnly: true,
+                            controller: _selectedDate != null
+                                ? TextEditingController(text: '${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}')
+                                : null,
+                            decoration: InputDecoration(
+                              labelText: 'Dia',
+                              border: OutlineInputBorder(),
+                            ),
+                            validator: (value) {
+                              if (_selectedDate == null) {
+                                return 'Por favor, selecione o dia do evento';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.calendar_today),
+                          onPressed: _selectDate,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            readOnly: true,
+                            controller: _selectedTime != null
+                                ? TextEditingController(text: '${_selectedTime!.hour}:${_selectedTime!.minute}')
+                                : null,
+                            decoration: InputDecoration(
+                              labelText: 'Hora',
+                              border: OutlineInputBorder(),
+                            ),
+                            validator: (value) {
+                              if (_selectedTime == null) {
+                                return 'Por favor, selecione a hora do evento';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.access_time),
+                          onPressed: _selectTime,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          String name = _nameController.text;
+                          String location = _locationController.text;
+                          String description = _descriptionController.text;
+                          String subtopic = _selectedSubtopic!;
 
-                        // Pode adicionar lógica para enviar os dados para um serviço ou API aqui
-                      }
-                    },
-                    child: const Text('Criar evento'),
-                  ),
-                ],
+                          DateTime? dateTime;
+                          if (_selectedDate != null && _selectedTime != null) {
+                            dateTime = DateTime(
+                              _selectedDate!.year,
+                              _selectedDate!.month,
+                              _selectedDate!.day,
+                              _selectedTime!.hour,
+                              _selectedTime!.minute,
+                            );
+                          }
+
+                          // Exemplo de uso dos dados (pode ser enviado para um serviço, etc.)
+                          print('Nome do Evento: $name');
+                          print('Local: $location');
+                          print('Descrição: $description');
+                          print('Subtópico: $subtopic');
+                          if (dateTime != null) {
+                            print('Data e Hora: $dateTime');
+                          }
+
+                          // Limpar formulário após envio
+                          _nameController.clear();
+                          _locationController.clear();
+                          _descriptionController.clear();
+                          setState(() {
+                            _selectedDate = null;
+                            _selectedTime = null;
+                            _image = null;
+                            _selectedSubtopic = null;
+                          });
+
+                          // Pode adicionar lógica para enviar os dados para um serviço ou API aqui
+                        }
+                      },
+                      child: const Text('Criar evento'),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
