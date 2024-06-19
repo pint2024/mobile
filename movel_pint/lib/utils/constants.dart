d7b6183bbfe5a443a1488791885248090c1a696c
Daniel <danielvalpereiro@gmail.com>
2024-06-19 12:14:00 +0100

    listar todos ainda n funfa :c

--- movel_pint/lib/utils/constants.dart
+++ movel_pint/lib/utils/constants.dart
@@ -1,4 +1,25 @@
 class CONSTANTS {
   static const String API_BASE_URL = 'http://localhost:8000';
   static const String API_MOBILE_BASE_URL = 'http://10.0.2.2:8000';
+
+  static const Map<String, Map<String, dynamic>> valores = {
+    'EVENTO': {
+      'ID': 1,
+      'TIPO': 'Evento',
+    },
+    'ATIVIDADE': {
+      'ID': 2,
+      'TIPO': 'Atividade',
+    },
+    'RECOMENDACAO': {
+      'ID': 3,
+      'TIPO': 'Recomendação',
+    },
+    'ESPACO': {
+      'ID': 4,
+      'TIPO': 'Espaço',
+    },
+  };
 }
+
+
