7d08ef1f52270ad66ac9283272707a9b7dc09463
Daniel <danielvalpereiro@gmail.com>
2024-07-02 21:36:35 +0100

    Add dos subtopicos

--- movel_pint/lib/backend/api_service.dart
+++ movel_pint/lib/backend/api_service.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:movel_pint/backend/myHttp.dart';
 import 'package:movel_pint/utils/constants.dart';
 
 class ApiService {
-
+  // Método para obter um recurso específico
   static Future<dynamic> obter(String endpoint, int id, {Map<String, String> headers = const {}}) async {
     final url = '$endpoint/obter/$id';
     try {
@@ -14,6 +15,7 @@ class ApiService {
     }
   }
 
+  // Método para listar recursos
   static Future<dynamic> listar(String endpoint, {Map<String, String> headers = const {}, dynamic data}) async {
     final url = '$endpoint/listar';
     try {
@@ -23,6 +25,36 @@ class ApiService {
     }
   }
 
+  static Future<http.Response> criarFormData(String endpoint, {Map<String, dynamic> data = const {}, String fileKey = "", Map<String, dynamic> headers = const {}}) async {
+    try {
+      var url = Uri.parse('/$endpoint/criar');
+
+      var request = http.MultipartRequest('POST', url);
+      
+      data.forEach((key, value) {
+        if (key != fileKey) {
+          request.fields[key] = value.toString();
+        }
+      });
+      
+      List<File> files = List<File>.from(data[fileKey]);
+      for (var file in files) {
+        var stream = http.ByteStream(file.openRead());
+        var length = await file.length();
+        var multipartFile = http.MultipartFile(fileKey, stream, length, filename: file.path.split('/').last);
+        request.files.add(multipartFile);
+      }
+      
+      var response = await http.Response.fromStream(await request.send());
+      
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+
+  // Método para criar um recurso
   static Future<dynamic> criar(String endpoint, {Map<String, String> headers = const {}, dynamic data}) async {
     final url = '$endpoint/criar';
     try {
@@ -32,12 +64,51 @@ class ApiService {
     }
   }
 
+  // Método para atualizar um recurso
+  static Future<void> atualizar(String endpoint, int id, {Map<String, String>? headers, dynamic data}) async {
+    final url = '$endpoint/atualizar/$id';
+    try {
+      final response = await http.put(
+        Uri.parse('$baseUrl/$url'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          ...?headers,
+        },
+        body: jsonEncode(data),
+      );
+      if (response.statusCode == 200) {
+        print('Recurso atualizado com sucesso');
+      } else {
+        throw Exception('Falha ao atualizar recurso: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Erro na conexão com o servidor: $e');
+    }
+  }
 
-  static Future<dynamic> atualizar(String endpoint, int id, {Map<String, String>? headers, dynamic data}) async {}
-  static Future<dynamic> remover(String endpoint, int id, {Map<String, String>? headers}) async {}
+  // Método para remover um recurso
+  static Future<void> remover(String endpoint, int id, {Map<String, String>? headers}) async {
+    final url = '$endpoint/remover/$id';
+    try {
+      final response = await http.delete(
+        Uri.parse('$baseUrl/$url'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          ...?headers,
+        },
+      );
+      if (response.statusCode == 200) {
+        print('Recurso removido com sucesso');
+      } else {
+        throw Exception('Falha ao remover recurso: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Erro na conexão com o servidor: $e');
+    }
+  }
 
-
-    static Future<Map<String, dynamic>?> postData(String endpoint, Map<String, dynamic> data) async {
+  // Método genérico para fazer requisições POST
+  static Future<Map<String, dynamic>?> postData(String endpoint, Map<String, dynamic> data) async {
     try {
       final response = await http.post(
         Uri.parse('$baseUrl/$endpoint'),
@@ -57,27 +128,16 @@ class ApiService {
     }
   }
 
+  // Método para atualizar perfil
   static Future<void> updateProfile(Map<String, dynamic> profileData) async {
     final String endpoint = 'utilizador/atualizar/${profileData['id']}';
     try {
-      // Preparar dados atualizados do perfil
-      Map<String, dynamic> updatedProfile = {
-        'nome': profileData['nome'],
-        'sobrenome': profileData['sobrenome'],
-        'email': profileData['email'],
-        'linkedin': profileData['linkedin'],
-        'facebook': profileData['facebook'],
-        'instagram': profileData['instagram'],
-        // Se o campo 'centro' for um ID numérico, converta para um formato aceitável na API
-        // Exemplo: 'centro': profileData['utilizador_centro']['centro']
-      };
-
       final response = await http.put(
         Uri.parse('$baseUrl/$endpoint'),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
-        body: jsonEncode(updatedProfile),
+        body: jsonEncode(profileData),
       );
 
       if (response.statusCode == 200) {
@@ -90,8 +150,7 @@ class ApiService {
     }
   }
 
-
- // Método genérico para fazer requisições PUT
+  // Método genérico para fazer requisições PUT
   static Future<Map<String, dynamic>?> putData(String endpoint, Map<String, dynamic> data) async {
     try {
       final response = await http.put(
@@ -131,5 +190,4 @@ class ApiService {
       throw Exception('Erro ao remover dados');
     }
   }
-
 }
